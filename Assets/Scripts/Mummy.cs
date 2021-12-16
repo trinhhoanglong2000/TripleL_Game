5b40677fa8394a950fd2c1ed82cec606ec381faa
Trịnh Hoàng Long <trinhhoanglong200@gmail.com>
2021-12-16 21:56:12 +0700

    add Mummy WakeUp sound

--- Assets/Scripts/Mummy.cs
+++ Assets/Scripts/Mummy.cs
@@ -5,45 +5,60 @@ using Pathfinding;
 
 public class Mummy : MonoBehaviour
 {
-
+    public AudioClip AWakeClip;
+    public float waitTime = 3f;
     Rigidbody2D rb;
     AIPath aIPath;
     Animator animator;
     Vector2 lookDirection = new Vector2(0, -1);
 
     private bool Sleep = true;
-
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
 
         aIPath = GetComponent<AIPath>();
-        
+        audioSource = GetComponent<AudioSource>();
+
+
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        
+
         if (!Sleep)
         {
-            //Debug.Log(aIPath.maxSpeed);
-            aIPath.maxSpeed = 1;
+
         }
         else
         {
 
         }
         lookDirection = aIPath.desiredVelocity.normalized;
-        
+
         animator.SetFloat("Look X", lookDirection.x);
         animator.SetFloat("Look Y", lookDirection.y);
-        animator.SetFloat("Speed",lookDirection.magnitude);
+        animator.SetFloat("Speed", lookDirection.magnitude);
 
     }
+    IEnumerator Awaking()
+    {
+        PlaySound(AWakeClip);
+        yield return new WaitForSeconds(waitTime);
+        //set Movement speed
+        aIPath.maxSpeed = 1;
 
+
+    }
+    public void PlaySound(AudioClip clip)
+    {
+        Debug.Log("HEHE");
+        audioSource.PlayOneShot(clip);
+    }
 
     void OnTriggerStay2D(Collider2D other)
     {
@@ -57,7 +72,10 @@ public class Mummy : MonoBehaviour
 
             if (hit.collider == null)
             { // null mean the raycast didn't hit the wall
+                if (Sleep)
+                    StartCoroutine(Awaking());
                 Sleep = false;
+
                 Debug.Log("Too much Light!!");
             }
 
