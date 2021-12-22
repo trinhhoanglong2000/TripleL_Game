8ff2427a2c497abe78e7a03a86a1c7a90202cdc7
Trần Phi Long <50954310+goku2000vn@users.noreply.github.com>
2021-12-22 21:21:50 +0700

    Audio and update menu sound

---
+++ Assets/Scripts/MenuScript/TextChange.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+using UnityEngine.Events; 
+using UnityEngine.EventSystems; 
+ 
+public class TextChange : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{ 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        GetComponent<Text>().color = Color.red; 
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        GetComponent<Text>().color = Color.black; 
+    }
+    public void FillBlackText()
+    {
+        GetComponent<Text>().color = Color.black;
+    }
+}
