4299f167e43361e3b28200da108ace38879d909e
Trần Phi Long <50954310+goku2000vn@users.noreply.github.com>
2022-01-09 17:04:32 +0700

    Add Change Volume

--- Assets/Scripts/MenuScript/MainScreenScript.cs
+++ Assets/Scripts/MenuScript/MainScreenScript.cs
@@ -1,10 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainScreenScript : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+    private void Start()
+    {
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+        }
+        Load();
+    }
     public void StartGame()
     {
         LoadScene(3);
@@ -12,6 +23,15 @@ public class MainScreenScript : MonoBehaviour
    public void LoadScene(int index)
     {
         SceneManager.LoadScene(index);
+    }
+    public void SetVolume(float silderValue)
+    {
+        audioMixer.SetFloat("volume",Mathf.Log10(silderValue)*20);
+        PlayerPrefs.SetFloat("musicVolume", silderValue);
+    }
+    public void Load() 
+    {
+        audioMixer.SetFloat("volume" , PlayerPrefs.GetFloat("musicVolume"));
     }
     public void QuitGame()
     {
